033f027798c4b88e181890b03a8ea102456c6146
xrenna <renna961596@163.com>
2020-09-22 07:52:02 +0800

    adds reducer for currentUser: :

---
+++ src/reducers/currentUser.js
@@ -0,0 +1,12 @@
+import { SET_CURRENT_USER, CLEAR_CURRENT_USER } from "../actionTypes"
+
+export function currentUserReducer(state = null, action) {
+    switch(action.type) {
+      case SET_CURRENT_USER:
+        return action.user
+      case CLEAR_CURRENT_USER:
+        return null
+      default:
+        return state
+    }
+  }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
+import { currentUserReducer } from './currentUser'
 import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
+    currentUser: currentUserReducer
  })
   
   export default rootReducer
