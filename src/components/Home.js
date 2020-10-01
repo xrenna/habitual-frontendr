ac56af736b20521ac70c8355e40170c1bac8dcc9
xrenna <renna961596@163.com>
2020-10-01 10:40:44 +0800

    adds component for buttons, animations, and styles header

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react'
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                Hello from Home. 
-            </div>
+            <header className="header">
+                <div className='header__text-box'>
+                    <h1 className='heading-primary'>
+                    <span className='heading-primary--main'>Habitual</span>
+                    <span className='heading-primary--sub'>A place for change</span>
+                    </h1>
+                </div>
+                
+                <div className='header__btn'>
+                    <button className="btn">Sign up Now</button>
+                </div>
+            </header>
         )
     }
 }
