363feb51f6490d661935b0588f069d3914a06146
xrenna <renna961596@163.com>
2020-11-18 17:02:27 +0800

    updates combined reducer to include habits

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { currentUserReducer } from './currentUser'
+import {goalsReducer} from './goals'
+import { habitsReducer } from './habits'
 import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
-    currentUser: currentUserReducer
+    currentUser: currentUserReducer,
+    goals: goalsReducer,
+    habits: habitsReducer
  })
   
   export default rootReducer
