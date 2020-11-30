9c33e730f43d26ccbaede04b8f52f73cc961e326
xrenna <renna961596@163.com>
2020-11-30 22:43:46 +0800

    adds styling to icons

--- src/containers/GoalsSidebar.js
+++ src/containers/GoalsSidebar.js
@@ -1,6 +1,7 @@
 import Goals from '../components/Goals'
 import { connect } from 'react-redux'
 import { deleteGoal } from '../actions/goals'
+import sprite from '../imgs/sprite.svg'
 
 
  function GoalsSidebar (props) {
@@ -10,12 +11,29 @@ import { deleteGoal } from '../actions/goals'
 
         const renderGoals = () => {
             return props.goals && props.goals.map(goal => 
-                <li className='side-nav__item' 
-                onClick={() => props.onClick(goal.id)} key={goal.id}>
-                <Goals {...goal}/>
-                <span><button onClick={() => props.populateForm(goal)}>Edit</button></span>
-                <span><button onClick={() => deleteGoal(goal)}>Delete</button></span>
-                </li>)
+                    <li className='side-nav__item' 
+                        onClick={() => props.onClick(goal.id)} key={goal.id}>
+                        <Goals {...goal}/>
+                        <span>
+                            <span className='side-nav__icon'>
+                                <a onClick={() => deleteGoal(goal)}>
+                                    <svg className="icon icon--trash">
+                                        <use href={sprite + '#icon-trash'} />
+                                    </svg>
+                                </a>
+                                
+                            </span>
+                            <span>
+                                <a onClick={() => props.populateForm(goal)}>
+                                    <svg className="icon icon--edit">
+                                        <use href={sprite + '#icon-edit'} />
+                                    </svg>
+                                </a>
+                            </span>
+                        </span>
+                    </li>
+                )
+                
         }
 
         return (
@@ -23,7 +41,14 @@ import { deleteGoal } from '../actions/goals'
             <ul className = 'side-nav'>
                 {renderGoals()}
             </ul>
-            <button onClick={props.openNewGoalModal}>+</button>
+            <span>
+                <a href="#" onClick={props.openNewGoalModal}>
+                    <svg className="icon icon--add">
+                        <use href={sprite + '#icon-plus'} />
+                    </svg>
+                </a>
+                <span>ADD GOAL</span>
+            </span>
             </>
         )
    
