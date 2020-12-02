d84d2735a8696efa07212d689332d3fb07b2aced
xrenna <renna961596@163.com>
2020-12-02 12:34:23 +0800

    adds gradient to card front dashboard, working on styling back of card

--- src/components/GoalCard.js
+++ src/components/GoalCard.js
@@ -12,8 +12,8 @@ function GoalCard (props) {
                 <h3 className='card__heading'>{props.name}</h3>
             </div>
             <div className='card__side card__side--back'>
-                <h5 className='card__details'>{props.name}'s Habits</h5>
-                <ul>
+                <header className='heading-tertiary'>{props.name}'s Habits</header>
+                <ul className='card__details'>
                     <li>{renderHabits()}</li>
                 </ul>
             </div>
