a0e543903248481503f0222e5cc07b323d6c4bf0
xrenna <renna961596@163.com>
2021-02-10 01:46:51 +0800

    adds new sprites for features section

--- src/components/Features.js
+++ src/components/Features.js
@@ -5,10 +5,16 @@ const Features = () => {
     return (
         <section className='features'>
             <div className='features__card'>
-            <svg className="features__icon">
-                <use href={sprite + '#icon-text-document'} />
-            </svg>
-            <p className=''></p>
+                <svg className="features__icon">
+                    <use href={sprite + '#icon-text-document'} />
+                </svg>
+                <p className='features__text'>Set weekly goals to establish new habits and improve yourself</p>
+            </div>
+            <div className='features__card'>
+                <svg className="features__icon">
+                    <use href={sprite + '#icon-text-document'} />
+                </svg>
+                <p className='features__text'>Set weekly goals to establish new habits and improve yourself</p>
             </div>
         </section>
     )
