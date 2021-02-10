956c91aa5850e6dd1f66d8ef511777a32ca30af8
xrenna <renna961596@163.com>
2021-02-11 03:52:23 +0800

    completes jsx markup for features

--- src/components/Features.js
+++ src/components/Features.js
@@ -12,9 +12,15 @@ const Features = () => {
             </div>
             <div className='features__card'>
                 <svg className="features__icon">
-                    <use href={sprite + '#icon-text-document'} />
+                    <use href={sprite + '#icon-hair-cross'} />
                 </svg>
-                <p className='features__text'>Set weekly goals to establish new habits and improve yourself</p>
+                <p className='features__text'>Target new habits in different facets of your life to become a better you</p>
+            </div>
+            <div className='features__card'>
+                <svg className="features__icon">
+                    <use href={sprite + '#icon-stopwatch'} />
+                </svg>
+                <p className='features__text'>A new habit forms in just 21 days, let Habitual be the beginning of this journey</p>
             </div>
         </section>
     )
