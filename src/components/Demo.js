5af89dc73d4169b0f5a5c857404c7c406d370ebe
xrenna <renna961596@163.com>
2021-02-20 23:51:22 +0800

    builds out jsx for demo__description

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -3,7 +3,17 @@ import React from 'react'
 const Demo = () => {
     return (
         <section className='demo'>
-            
+            <div className='demo__description'>
+                <p className='demo__text'>
+                    Habitual is a habit tracker SPA that allows users to track their progress, set frequency by week, and manage their goals for personal growth.
+                </p>
+                <p className='demo__text'>
+                    Engineered the UI using React.js and Redux to manage the state of application & used Redux and Thunk middleware to retrieve data and implement data persistence.
+                </p>
+                <p className='demo__text'>
+                    Implemented Sass as a CSS precompiler to efficiently design the UX.
+                </p>
+            </div>
         </section>
     )
 }
