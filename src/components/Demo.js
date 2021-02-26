a8c6f71eba7cb1a4dadb7ab47216497d3d9d85a3
xrenna <renna961596@163.com>
2021-02-26 15:36:17 +0800

    working on grid elements within section demo

--- src/components/Demo.js
+++ src/components/Demo.js
@@ -3,21 +3,22 @@ import React from 'react'
 const Demo = () => {
     return (
         <section className='demo'>
+            <h3 className='demo__header'>About Habitual</h3>
             <div className='demo__description'>
-                <p className='demo__text'>
+                <p className='text text--1'>
                     Habitual is a habit tracker SPA that allows users to track their progress, set frequency by week, and manage their goals for personal growth.
                 </p>
-                <p className='demo__text'>
+                <p className='text text--2'>
                     Engineered the UI using React.js and Redux to manage the state of application & used Redux and Thunk middleware to retrieve data and implement data persistence.
                 </p>
-                <p className='demo__text'>
+                <p className='text text--3'>
                     Implemented Sass as a CSS precompiler to efficiently design the UX.
                 </p>
             </div>
             <div className='demo__box'>
                 Will hold the dif of the demo eventually 
             </div>
-            <button className='btn'>Try it out now</button>
+            <button className='btn demo__btn'>Try it out now</button>
         </section>
     )
 }
